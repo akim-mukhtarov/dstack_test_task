28012036f289fae62b36f1dee6fa073b438bf3b7
Akim Mukhtarov <akim.int80h@gmail.com>
2023-02-06 22:54:54 +0300

    add script to run a program with test input

--- test.sh
+++ test.sh
@@ -2,12 +2,12 @@
 
 
 python3 main.py \
-    --docker-image python \
-    --bash-command 'pip install pip -U && pip
+    --docker-image test-image \
+    --bash-command $'pip install pip -U && pip
 install tqdm && python -c \"import time\ncounter = 0\nwhile
 True:\n\tprint(counter)\n\tcounter = counter + 1\n\ttime.sleep(0.1)\"' \
     --aws-cloudwatch-group test-task-group-1 \
     --aws-cloudwatch-stream test-task-stream-1 \
     --aws-access-key-id $AWS_ACCESS_KEY_ID \
     --aws-secret-access-key $AWS_SECRET_ACCESS_KEY \
-    --aws-region ap-northeast-1
+    --aws-region $AWS_REGION
