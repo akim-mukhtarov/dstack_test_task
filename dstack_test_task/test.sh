1f844fd46d01afa0ebda0342b6eb5c7de0f064af
Akim Mukhtarov <akim.int80h@gmail.com>
2023-02-07 16:05:34 +0300

    fix multiline bash command

--- dstack_test_task/test.sh
+++ dstack_test_task/test.sh
@@ -3,9 +3,7 @@
 
 python3 main.py \
     --docker-image test-image \
-    --bash-command $'pip install pip -U && pip
-install tqdm && python -c \"import time\ncounter = 0\nwhile
-True:\n\tprint(counter)\n\tcounter = counter + 1\n\ttime.sleep(0.1)\"' \
+    --bash-command $'pip install pip -U && pip install tqdm && python -c \"import time\ncounter = 0\nwhile True:\n\tprint(counter)\n\tcounter = counter + 1\n\ttime.sleep(0.1)\"' \
     --aws-cloudwatch-group test-task-group-1 \
     --aws-cloudwatch-stream test-task-stream-1 \
     --aws-access-key-id $AWS_ACCESS_KEY_ID \
